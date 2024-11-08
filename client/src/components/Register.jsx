f5ebfb24dad10e1dfbef1d917865c091b9fb5abd
Sai Harshitha Akkina <120376983+harshitha-2201@users.noreply.github.com>
2024-11-08 21:24:15 +0530

    Update Register.jsx

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -13,7 +13,7 @@ const Register = () => {
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:3000/api/auth/register', { name, email, password, phone });
+      const response = await axios.post('https://appointment-booking-backend-7k26.onrender.com/api/auth/register', { name, email, password, phone });
       console.log(response.data);
       setSuccess('Registration successful!');
       setError('');
